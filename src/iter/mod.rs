02c98b6193a724e75d9d9b4cdab8a271ac268731
frederic wagner <frederic.wagner@imag.fr>
2021-04-12 10:34:27 +0200

    added walk_tree + doc fixes

--- src/iter/mod.rs
+++ src/iter/mod.rs
@@ -179,7 +179,9 @@ pub use self::{
     take::Take,
     try_fold::{TryFold, TryFoldWith},
     update::Update,
-    walk_tree::{walk_tree_postfix, walk_tree_prefix, WalkTreePostfix, WalkTreePrefix},
+    walk_tree::{
+        walk_tree, walk_tree_postfix, walk_tree_prefix, WalkTree, WalkTreePostfix, WalkTreePrefix,
+    },
     while_some::WhileSome,
     zip::Zip,
     zip_eq::ZipEq,
